ddea85464997a36372e9a28ad121b213c2e1b7cf
Craig Condon <craig@spiceapps.com>
2013-04-09 13:37:45 -0700

    replace browserify with sardines

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,1 +1,10 @@
-console.log("hello world!!");
+var http = require("http"),
+port = 1337;
+
+console.log(process.argv);
+
+console.log("started HTTP server on port %d", 1337);
+
+http.createServer(function(req, res) {
+  res.end("Hello World!!");
+}).listen(port);
