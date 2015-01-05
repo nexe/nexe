b615e125c055b6e8612ad13089de3244404d7a05
Lorenz Gardner <Lorenz03tx@gmail.com>
2015-01-05 17:58:57 -0600

    Make the example code demonstrate forked process and dynamic require

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -3,6 +3,14 @@ fs = require("fs"),
 path = require("path"),
 port = 1337;
 
+//Fork a new process to execute the broser.js
+var childProc = require("child_process");
+childProc.fork("./browser.js");
+if (false) {
+	require("./browser.js"); //force the bundler to include the .js file
+}
+
+//start a web server on specified port (or default if non given)
 console.log("started HTTP server on port %d", port = Number(process.argv.concat().pop()) || port);
 
 http.createServer(function(req, res) {
