f75948b7e84ce430b4e5a5e6622349a62178d1cb
Jared Allard <jaredallard@outlook.com>
2016-02-01 12:56:48 -0800

    exe: use custom tar+gz extraction method, should fix #175

---
+++ test/ignoreFlags-test/index.js
@@ -0,0 +1,19 @@
+/**
+ * Test to verify if we support strict mode (flags)
+ *
+ * @author Jared Allard <jaredallard@outlook.com>
+ * @version 0.0.1
+ * @license MIT
+ **/
+
+ // test.nex --help
+
+ var status = false;
+
+ // console.log(process.argv);
+
+ if(process.argv[2]) {
+   status = true;
+ }
+
+ console.log('test:ignoreFlags:status =', status);
