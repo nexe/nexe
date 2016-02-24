f50e94a781efe2302674174654a4ddccb8f90713
Jared Allard <jaredallard@outlook.com>
2016-02-23 23:00:19 -0800

    bundle/exe: more ES6, browserify.excludes, browserify.requires in package.json.

---
+++ test/embeded-files/index.js
@@ -0,0 +1,11 @@
+/**
+ * Embedded Files Test
+ *
+ * @author Jared Allard <jaredallard@outlook.com>
+ * @version 0.0.1
+ * @license MIT
+ **/
+
+const nexeres = require('nexeres');
+
+console.log(nexeres.get('hw.txt').toString('ascii'))
