4710382c0a5485c11f8381d58746fbdb2e8a1686
James M. Greene <james.m.greene@gmail.com>
2018-03-13 15:10:32 -0500

    fix: bootstrap file path in node 4.x

--- src/patches/third-party-main.ts
+++ src/patches/third-party-main.ts
@@ -4,7 +4,9 @@ import { join } from 'path'
 import { wrap } from '../util'
 
 export default async function main(compiler: NexeCompiler, next: () => Promise<void>) {
-  const file = await compiler.readFileAsync('lib/internal/bootstrap_node.js')
+  const bootPath =
+    (compiler.target.version.startsWith('4') ? 'src/' : 'lib/internal/bootstrap_') + 'node.js'
+  const file = await compiler.readFileAsync(bootPath)
   const parts = file.contents.split('function(process) {')
   file.contents =
     parts[0] + `function(process) {` + '{{replace:lib/patches/bootstrap.js}}' + parts[1]
