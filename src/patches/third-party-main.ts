ad41f07d85d8f372cfd2ea4ba074a99e92ade1cd
calebboyd <caleb.boyd@gmail.com>
2018-04-08 23:35:52 -0500

    chore: support node 10

--- src/patches/third-party-main.ts
+++ src/patches/third-party-main.ts
@@ -1,15 +1,31 @@
 import { NexeCompiler } from '../compiler'
 import { readFileSync } from 'fs'
+import semver = require('semver')
 import { join } from 'path'
 import { wrap } from '../util'
 
 export default async function main(compiler: NexeCompiler, next: () => Promise<void>) {
-  const bootPath =
-    (compiler.target.version.startsWith('4') ? 'src/' : 'lib/internal/bootstrap_') + 'node.js'
-  const file = await compiler.readFileAsync(bootPath)
-  const parts = file.contents.split('function(process) {')
-  file.contents =
-    parts[0] + `function(process) {` + '{{replace:lib/patches/bootstrap.js}}' + parts[1]
+  let bootFile = 'lib/internal/bootstrap_node.js'
+  const { version } = compiler.target
+
+  if (semver.satisfies(version, '4')) {
+    bootFile = 'src/node.js'
+  } else if (semver.gt(version, '9.10.1')) {
+    bootFile = 'lib/internal/boostrap/node.js'
+  }
+
+  const file = await compiler.readFileAsync(bootFile),
+    matches = file.contents.match(/\(function.*/),
+    functionHeader = matches && matches[0]
+
+  if (!functionHeader) {
+    throw new Error('Failed to find bootstrap header in node version: v' + version)
+  }
+
+  file.contents.replace(
+    functionHeader,
+    functionHeader + '\n' + '{{replace:lib/patches/bootstrap.js}}'
+  )
 
   await compiler.setFileContentsAsync(
     'lib/_third_party_main.js',
