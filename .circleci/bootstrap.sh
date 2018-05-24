995110a9fcde14f5d78f86f1ac3ec77aecdc29aa
Jared Allard <jaredallard@outlook.com>
2018-05-23 18:37:49 -0700

    fix(ci): keep nvs since it works

--- .circleci/bootstrap.sh
+++ .circleci/bootstrap.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
 echo "setting up node to use latest lts"
-npm install -g n
-n lts
+
+export NVS_HOME="$HOME/.nvs"
+git clone --depth=1 https://github.com/jasongin/nvs "$NVS_HOME"
+. "$NVS_HOME/nvs.sh" install
+nvs add lts
+nvs link lts
+nvs use default
+echo "using node $(node --version)"
 
 # Install latest yarn version
 npm install -g yarn
