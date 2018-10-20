68de6ead04d07ea95d0c24e1e2c2c335b5dc571d
António Nuno Monteiro <anmonteiro@gmail.com>
2018-10-20 16:50:21 +0100

    Include snapshot in patches

--- src/patches/index.ts
+++ src/patches/index.ts
@@ -5,7 +5,8 @@ import cli from './disable-node-cli'
 import flags from './flags'
 import ico from './ico'
 import rc from './node-rc'
+import snapshot from './snapshot'
 
-const patches = [gyp, bootNexe, buildFixes, cli, flags, ico, rc]
+const patches = [gyp, bootNexe, buildFixes, cli, flags, ico, rc, snapshot]
 
 export default patches
