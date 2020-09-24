0f0869b292f1d7b68ba6e170d628de68a10c009f
bruce-one <bryce@gibson-consulting.com.au>
2020-09-25 01:13:22 +1000

    fix: internal module read json changes 12.18.3 (#803)

--- src/fs/patch.ts
+++ src/fs/patch.ts
@@ -317,7 +317,14 @@ function shimFs(binary: NexeBinary, fs: any = require('fs')) {
     log('read          (miss)       ' + filepath)
     return original.call(this, ...args)
   }
-  patches.internalModuleReadJSON = patches.internalModuleReadFile
+  let returningArray: boolean
+  patches.internalModuleReadJSON = function (this: any, original: any, ...args: any[]) {
+    if (returningArray == null) returningArray = Array.isArray(original.call(this, ''))
+    const res = patches.internalModuleReadFile.call(this, original, ...args)
+    return returningArray && !Array.isArray(res)
+      ? [res, /"(main|name|type|exports|imports)"/.test(res)]
+      : res
+  }
   patches.internalModuleStat = function (this: any, original: any, ...args: any[]) {
     setupManifest()
     const filepath = getKey(args[0])
