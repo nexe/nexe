ad7346afb2344965dfd4298febf168b44b246604
calebboyd <caleb.boyd@gmail.com>
2022-12-23 13:32:04 -0600

    bump deps

--- src/patches/disable-node-cli.ts
+++ src/patches/disable-node-cli.ts
@@ -1,16 +1,16 @@
 import type { NexeCompiler } from '../compiler.js'
 import { NexeError } from '../compiler.js'
+import { semverGt } from '../util.js'
 
 export default async function disableNodeCli(compiler: NexeCompiler, next: () => Promise<void>) {
   if (compiler.options.enableNodeCli) {
     return await next()
   }
 
-  const replaced = await compiler.replaceInFileAsync(
-    'src/node.cc',
-    /(?<!int )ProcessGlobalArgs\(argv[^;]*;/gm,
-    '0;/*$&*/'
-  )
+  const [regex, replacement] = semverGt(compiler.target.version, '18.99')
+      ? [/(?<!int )ProcessGlobalArgsInternal\(argv[^;]*;/gm, 'ExitCode::kNoFailure']
+      : [/(?<!int )ProcessGlobalArgs\(argv[^;]*;/gm, 0],
+    replaced = await compiler.replaceInFileAsync('src/node.cc', regex, `${replacement};/*$&*/`)
 
   if (!replaced) {
     throw new NexeError('There was an error applying the Node CLI patch. Please open an issue.')
