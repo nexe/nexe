3c2df0d846a0d2fe4b3b8c370472a5e4c162eb85
Bryce Gibson <bryce@gibson-consulting.com.au>
2024-01-30 10:24:11 +1100

    Ignore false-failure in integration test cleanup.
    This shouldn't make the test execution fail.
    
    It seems to popup occasionally for some Windows removal racing, or similar.

--- test/integration/index.js
+++ test/integration/index.js
@@ -4,14 +4,23 @@ const path = require('path')
 const rimraf = require('rimraf')
 const cp = require('child_process')
 
+function cleanup(dir) {
+  try {
+    rimraf.sync(dir)
+  } catch(e) {
+    console.error(`Ignoring error during integration test cleanup of "${dir}".`)
+    console.error(`Error: ${e}`)
+  }
+}
+
 async function runTests() {
   const tempdir = await realpath(await mkdtemp(path.join(os.tmpdir(), 'nexe-integration-tests-')))
   const secondTempdir = await mkdtemp(path.join(os.tmpdir(), 'nexe-integration-tests-without-executable-'))
   const executable = path.join(tempdir, path.basename(process.argv[0]))
   await copyFile(process.argv[0], executable)
   process.on('beforeExit', () => {
-    rimraf.sync(tempdir)
-    rimraf.sync(secondTempdir)
+    cleanup(tempdir)
+    cleanup(secondTempdir)
   })
   console.error('Running integration tests with the binary in the current working directory.')
   spawnExecutable(tempdir, () => {
